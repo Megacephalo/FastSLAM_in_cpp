ce26f6ed4031ab405dddeffeeac15e41cd3ab191
Charly Huang <charly.charlongo@gmail.com>
2020-06-30 06:16:30 -0400

    Add unit test case for experimenting with plotting a line using matplotlibcpp
    And comment out the first test case to simplify the window display

--- tests/test_matplotlibcpp.cpp
+++ tests/test_matplotlibcpp.cpp
@@ -3,13 +3,27 @@
 #include <vector>
 
 #include "draw.h"
-
-TEST(matplotlib_test, ) {
+#if 0
+TEST(matplotlib_test, build_basic_frame_test) {
 	Draw drawer ;
 	drawer.draw_basic_frame() ;
 	drawer.plot_point(1, 2) ;
 	drawer.Show() ;
 }
+#endif
+TEST(matplotlib_test, plot_line_test) {
+	// plt::plot( {1, 3} ) ;
+
+	std::vector<double> X, Y ;
+
+	// Drw a line from (1, 1) to (5, 1)
+	X = {1, 5, } ;
+	Y = {1, 1} ;
+	plt::plot(X, Y) ;
+	plt::show() ;
+}
+
+
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv) ;
